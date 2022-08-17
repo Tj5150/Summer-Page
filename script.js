02fb582e7279e12dc44b0c9b2eea7bac09268072
Tj5150 <89049603+Tj5150@users.noreply.github.com>
2022-08-17 15:28:38 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById("main").style.backgroundColor = "dodgerblue";
+document.getElementById("main").style.color = "#F2B144";
+document.getElementById("main").style.fontFamily = 'DynaPuff', cursive;
